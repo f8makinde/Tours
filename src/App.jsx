464ae7d79cc3e740c01fc69b912c8fcdb7191c73
Faith Makinde <faithmakinde0@gmail.com>
2023-02-23 12:07:37 +0100

    installed react

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React, { useEffect, useState } from 'react'
+
+const url = 'https://course-api.com/react-tours-project'
+const App = () => {
+  const [isLoading, setIsLoading] = useState(true)
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default App
+
+
+
